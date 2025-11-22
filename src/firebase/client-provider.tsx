ebf101bb1dbc662d521ce259c08744897b6f406c
GABRIEL FABIAN SANCHEZ MAMANI <76197162@continental.edu.pe>
2025-11-22 01:56:48 +0000

    lanza este error vercel chequealo Deployment Deployment failed with erro

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -3,18 +3,18 @@ import { useMemo } from 'react';
 import { FirebaseProvider } from './provider';
 import { initializeFirebase } from './index';
 
-const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
-
-
 export function FirebaseClientProvider({ children }: { children: React.ReactNode }) {
-  const { app, auth, firestore } = useMemo(() => initializeFirebase(firebaseConfig), []);
+  
+  const firebaseConfig = {
+      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  };
+
+  const { app, auth, firestore } = useMemo(() => initializeFirebase(firebaseConfig), [firebaseConfig]);
 
   return (
     <FirebaseProvider app={app} auth={auth} firestore={firestore}>
