54f2c5daeee2fa3d9b40e00089adc2160a76c994
MISHELL MARICIELO HERMOZA CONCHA <73005420@continental.edu.pe>
2025-11-26 18:32:15 +0000

    La razón por la que Firebase falla es que tu archivo .env tiene la clave

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,12 +1,11 @@
-
 // /src/firebase/config.ts
 
-// IMPORTANT: These are the hardcoded credentials for debugging purposes.
+// Esta configuración ahora lee las variables de entorno públicas de Next.js
 export const firebaseConfig = {
-  apiKey: "AIzaSyCCmb7V4gIUSLJgQzyM1OwsE8SC9g8c", 
-  authDomain: "studio-9478792464-25eb9.firebaseapp.com", 
-  projectId: "studio-9478792464-25eb9",
-  storageBucket: "studio-9478792464-25eb9.firebaseapp.com", 
-  messagingSenderId: "182565684029", 
-  appId: "1:182565684029:web:73e237b72e049f6b843b54",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
